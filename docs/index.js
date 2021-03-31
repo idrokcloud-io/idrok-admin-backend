d1e601fb3e32be41330daf638d14f1cf4fe9efa4
MLRDer <r.qirgizboyev@mail.ru>
2021-03-31 15:14:45 +0500

    user route added

--- docs/index.js
+++ docs/index.js
@@ -5,17 +5,20 @@ const productRoutes = require("./routes/product.json");
 const adRoutes = require("./routes/ads.json");
 const brandRoutes = require("./routes/brand.json");
 const carRoutes = require("./routes/car.json");
+const userRoutes = require("./routes/user.json");
 
 const productModels = require("./models/product.json");
 const adModels = require("./models/ads.json");
 const brandModels = require("./models/brand.json");
 const carModels = require("./models/car.json");
+const userModels = require("./models/user.json");
 
 const paths = {
     ...productRoutes,
     ...adRoutes,
     ...brandRoutes,
     ...carRoutes,
+    ...userRoutes,
 };
 
 const definitions = {
@@ -23,6 +26,7 @@ const definitions = {
     ...adModels,
     ...brandModels,
     ...carModels,
+    ...userModels,
 };
 
 module.exports = {
